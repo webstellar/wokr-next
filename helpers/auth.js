2a68fe93d371e635f11af7212422acdcce4cd9f2
Peter Onyegbule <49274497+webstellar@users.noreply.github.com>
2024-02-09 10:02:44 +0100

    Update auth.js

--- helpers/auth.js
+++ helpers/auth.js
@@ -3,15 +3,15 @@ import { getAuth } from "firebase-admin/auth";
 
 import serviceAccount from "../config/_fbServiceAccountKey.json" assert { type: "json" };
 
-// Initialize the Firebase app with credentials
-const app = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
+if (!admin.apps.length) {
+  const app = admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 // Auth check function
-export const authCheck = async (req) => {
+export const authCheck = async (token) => {
   try {
-    const currentUser = await getAuth().verifyIdToken(req.headers.authtoken);
+    const currentUser = await getAuth().verifyIdToken(token);
     console.log("CURRENT USER", currentUser);
     return currentUser;
   } catch (error) {
